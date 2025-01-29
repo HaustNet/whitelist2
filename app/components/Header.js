620e03d9a41c5d9647bfac3f883e99647006bb4b
Bees <137223212+HaustNet@users.noreply.github.com>
2025-01-29 15:02:02 +0700

    Closed WL

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,26 +1,10 @@
 'use client';
-
-import { ConnectKitButton } from "connectkit";
-
 export default function Header() {
     return (
         <header className="header">
             <img className="logo" src="/logo.svg" alt="Haust Labs Logo" width={189} height={28} />
             <div className="wallet-container">
-                <ConnectKitButton.Custom>
-                    {({ isConnected, show, address }) => (
-                        <button
-                            className={isConnected
-                                ? "wallet-add "
-                                : "wallet-button"}
-                            onClick={show} // Открывает окно выбора кошелька
-                        >
-                            {isConnected
-                                ? `${address.slice(0, 6)}...${address.slice(-4)}`
-                                : "Connect Wallet"}
-                        </button>
-                    )}
-                </ConnectKitButton.Custom>
+              
             </div>
         </header>
     );
